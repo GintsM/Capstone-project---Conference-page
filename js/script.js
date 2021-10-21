5137779f3439674abbbe2d7bce08da8670a7ccc6
Gints Misins <88575586+GintsM@users.noreply.github.com>
2021-10-21 10:02:12 +0300

    Dynamic html part with participants with javascript

--- js/script.js
+++ js/script.js
@@ -14,3 +14,86 @@ function showMenu() {
 hamburger.addEventListener('click', showMenu);
 closeElemetn.addEventListener('click', showMenu);
 menuLinks.forEach((li) => li.addEventListener('click', showMenu));
+
+// Featured Speakers
+const speakers = [
+  {
+    name: 'Gints Misins',
+    occup: 'SIA Pamati majai, consultant, created own matrix, freelancer',
+    img: 'images/portr1.png',
+    description: 'The mechanical and durability properties of UHPC make it an ideal candidate for use in developing new solutions to pressing concerns about highway infrastructure deterioration, repair, and replacement',    
+  },
+  {
+    name: 'Migins Simnts',
+    occup: 'New York State department of transportation (DOT)',
+    img: 'images/portr2.png',
+    description: 'UHPC is a cementitious composite material composed of an optimized gradation of granular constituents The mechanical and durability properties of UHPC make it an ideal candidate for use.',    
+  },
+  {
+    name: 'Sandra Forshina',
+    occup: 'Senior Finansial Analyst  SIA Pamati majai',
+    img: 'images/forshina.png',
+    description: 'UHPC is a cementitious composite material composed of an optimized gradation of granular constituents, a water-to-cementitious . Infrastructure deterioration, repair, and replacement',    
+  },
+  {
+    name: 'Sindija Sauliete',
+    occup: 'Global market manager, UHPC research',
+    img: 'images/f2.png',
+    description: 'UHPC is being considered for use in a wide variety of highway infrastructure applications. The high compressive and tensile strengths',    
+  },
+  {
+    name: 'Elizabhete Sofia Kaimina',
+    occup: 'Student Jelagava technology college',
+    img: 'images/meita.png',
+    description: 'high performance concretes have demonstrated exceptional performance when used as a field-cast closure pour or grout material',    
+  },
+  {
+    name: 'Gints Misins',
+    occup: 'SIA Pamati majai, consultant, created own matrix, freelancer',
+    img: 'images/portr1.png',
+    description: 'The mechanical and durability properties of UHPC make it an ideal candidate for use in developing new solutions to pressing concerns about highway infrastructure deterioration, repair, and replacement',
+    
+}];
+
+const startPoint = document.querySelector('.dynamic');
+const programTitle = document.createElement('h4');
+programTitle.classList.add('programtitle.speakers');
+const underLine = document.createElement('span');
+underLine.classList.add('underline');
+const speakContainer = document.createElement('div');
+speakContainer.classList.add('speakConteiner');
+startPoint.appendChild(programTitle);
+startPoint.appendChild(underLine);
+startPoint.appendChild(speakContainer);
+
+for (let i = 0; i < speakers.length; i += 1){
+  const indvidCont = document.createElement('div');
+  indvidCont.classList.add('indvidcont');
+  speakContainer.appendChild(indvidCont);
+  const imageContainer = document.createElement('div');
+  imageContainer.classList.add('imgparticipant');
+  indvidCont.appendChild(imageContainer);
+  const image = document.createElement('img');
+  image.setAttribute('src', speakers[i].img);
+  image.setAttribute('alt', 'Speaker Portret');
+  imageContainer.appendChild(image);
+  const participant = document.createElement('div');
+  participant.classList.add('participant');
+  indvidCont.appendChild(participant);
+  const nameParticiant = document.createElement('h4');
+  nameParticiant.innerHTML = speakers[i].name;
+  nameParticiant.classList.add('partName');
+  const position = document.createElement('p');
+  position.classList.add('position');
+  const italic = document.createElement('i');
+  italic.innerHTML = speakers[i].occup;
+  position.appendChild(italic);
+  const splitLine = document.createElement('hr');
+  const description = document.createElement('p');
+  description.innerHTML = speakers[i].description;
+  description.classList.add('position');
+  participant.appendChild(nameParticiant);
+  participant.appendChild(position);
+  participant.appendChild(splitLine);
+  participant.appendChild(description);
+}
